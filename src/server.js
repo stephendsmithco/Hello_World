467e50a65a8b95a5d2d1f863e46fcd05477ac506
Stephen Smith <stephendsmithco@gmail.com>
2019-09-15 19:40:36 -0600

    Add swagger documentation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerDoc = require('./swagger.json')
 const port = process.env.PORT || 3000
 const helloWorldController = require('./controllers/hello_world_controller')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.get('/helloWorld', (req, res) => {
   helloWorldController.getHelloWorld(req, res)
